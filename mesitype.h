295325859710263118a32a421203c0c441816700
Jameson Thatcher <sireel@bluescreenofdoom.com>
2013-05-13 23:21:51 +0100

    maybe it'll work this time

--- mesitype.h
+++ mesitype.h
@@ -1,18 +1,32 @@
 #include <string>
 
-/*
- * TODO
- *
- * Unary operators (+ and -)
- * comparison operators (all of them)
- * scale by T
- */
+// TODO add copyright notice?
 
+/**
+ * @brief Main class to store SI types
+ *
+ * @param T storage type parameter
+ * @param t_m number of dimensions of meters, e.g., t_m == 2 => m^2
+ * @param t_s similar to t_m, but seconds
+ * @param t_kg similar to t_m, but kilograms
+ *
+ * This class is to enforce compile-time checking, and where possible,
+ * compile-time calculation of SI values using constexpr.
+ * 
+ * Note: MESI_LITERAL_TYPE may be defined to set the storage type
+ * used by the operator literal overloads
+ *
+ * @author Jameson Thatcher (a.k.a. SirEel)
+ *
+ */
 template<typename T, int t_m, int t_s, int t_kg>
 struct MesiType {
 	T val;
 
-	constexpr MesiType(T in)
+	constexpr MesiType()
+	{}
+
+	constexpr explicit MesiType(T const in)
 		:val(in)
 	{}
 
@@ -20,21 +34,31 @@ struct MesiType {
 		:val(in.val)
 	{}
 
-	std::string getUnit() const {
-		std::string ret = "";
+
+	/**
+	 * getUnit will get a SI-style unit string for this class
+	 */
+	static std::string getUnit() {
+		static std::string s_unitString("");
+		if( s_unitString.size() > 0 )
+			return  s_unitString;
+
 		if( t_m == 1 )
-			ret += "m ";
+			s_unitString += "m ";
 		else if( t_m != 0 )
-			ret += "m^" + std::to_string(t_m) + " ";
+			s_unitString += "m^"
+				+ std::to_string(static_cast<long long>(t_m)) + " ";
 		if( t_s == 1 )
-			ret += "s ";
+			s_unitString += "s ";
 		else if( t_s != 0 )
-			ret += "s^" + std::to_string(t_s) + " ";
+			s_unitString += "s^"
+				+ std::to_string(static_cast<long long>(t_s)) + " ";
 		if( t_kg == 1 )
-			ret += "kg ";
+			s_unitString += "kg ";
 		else if( t_kg != 0 )
-			ret += "kg^" + std::to_string(t_kg) + " ";
-		return ret.substr(0, ret.size() - 1);
+			s_unitString += "kg^"
+				+ std::to_string(static_cast<long long>(t_kg)) + " ";
+		return s_unitString.substr(0, s_unitString.size() - 1);
 	}
 
 	MesiType<T, t_m, t_s, t_kg>& operator+=(MesiType<T, t_m, t_s, t_kg> const& rhs) {
@@ -44,48 +68,186 @@ struct MesiType {
 	MesiType<T, t_m, t_s, t_kg>& operator-=(MesiType<T, t_m, t_s, t_kg> const& rhs) {
 		return (*this) = (*this) - rhs;
 	}
+
+	MesiType<T, t_m, t_s, t_kg>& operator*=(T const& rhs) {
+		return (*this) = (*this) * rhs;
+	}
+
+	MesiType<T, t_m, t_s, t_kg>& operator /=(T const& rhs) {
+		return (*this) = (*this) / rhs;
+	}
 };
+
+/*
+ * Arithmatic operators for combining SI values.
+ */
 
 template<typename T, int t_m, int t_s, int t_kg>
 constexpr MesiType<T, t_m, t_s, t_kg> operator+(
-		MesiType<T, t_m, t_s, t_kg> const left,
-		MesiType<T, t_m, t_s, t_kg> const right
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		MesiType<T, t_m, t_s, t_kg> const& right
 ) {
 	return MesiType<T, t_m, t_s, t_kg>(left.val + right.val);
 }
 
 template<typename T, int t_m, int t_s, int t_kg>
 constexpr MesiType<T, t_m, t_s, t_kg> operator-(
-		MesiType<T, t_m, t_s, t_kg> const left,
-		MesiType<T, t_m, t_s, t_kg> const right
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		MesiType<T, t_m, t_s, t_kg> const& right
 ) {
 	return MesiType<T, t_m, t_s, t_kg>(left.val - right.val);
 }
 
 template<typename T, int t_m, int t_m2, int t_s, int t_s2, int t_kg, int t_kg2>
 constexpr MesiType<T, t_m + t_m2, t_s + t_s2, t_kg + t_kg2> operator*(
-		MesiType<T, t_m, t_s, t_kg> const left,
-		MesiType<T, t_m2, t_s2, t_kg2> const right
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		MesiType<T, t_m2, t_s2, t_kg2> const& right
 ) {
 	return MesiType<T, t_m + t_m2, t_s + t_s2, t_kg + t_kg2>(left.val * right.val);
 }
 
 template<typename T, int t_m, int t_m2, int t_s, int t_s2, int t_kg, int t_kg2>
 constexpr MesiType<T, t_m - t_m2, t_s - t_s2, t_kg - t_kg2> operator/(
-		MesiType<T, t_m, t_s, t_kg> const left,
-		MesiType<T, t_m2, t_s2, t_kg2> const right
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		MesiType<T, t_m2, t_s2, t_kg2> const& right
 ) {
 	return MesiType<T, t_m - t_m2, t_s - t_s2, t_kg - t_kg2>(left.val / right.val);
 }
 
-constexpr MesiType<float, 1, 0, 0> operator "" _m(long double arg) {
-	return MesiType<float, 1, 0, 0>(arg);
+/*
+ * Unary operators, to help with literals (and general usage!)
+ */
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, t_m, t_s, t_kg> operator-(
+		MesiType<T, t_m, t_s, t_kg> const& op
+) {
+	return MesiType<T, t_m, t_s, t_kg>(-op.val);
 }
 
-constexpr MesiType<float, 0, 1, 0> operator "" _s(long double arg) {
-	return MesiType<float, 0, 1, 0>(arg);
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, t_m, t_s, t_kg> operator+(
+		MesiType<T, t_m, t_s, t_kg> const& op
+) {
+	return op;
 }
 
-constexpr MesiType<float, 0, 0, 1> operator "" _kg(long double arg) {
-	return MesiType<float, 0, 0, 1>(arg);
+/*
+ * Scalers by non-SI values (allows things like 2 * 3._m
+ */
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, t_m, t_s, t_kg> operator*(
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		T const& right
+) {
+	return MesiType<T, t_m, t_s, t_kg>(left.val * right);
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, t_m, t_s, t_kg> operator*(
+		T const & left,
+		MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return right * left;
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, t_m, t_s, t_kg> operator/(
+		MesiType<T, t_m, t_s, t_kg> const& left,
+		T const& right
+) {
+	return MesiType<T, t_m, t_s, t_kg>(left.val / right);
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr MesiType<T, -t_m, -t_s, -t_kg> operator/(
+		T const & left,
+		MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return MesiType<T, -t_m, -t_s, -t_kg>( left * right.val );
+}
+
+/*
+ * Comparison operators
+ */
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator==(
+	MesiType<T, t_m, t_s, t_kg> const& left,
+	MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return left.val == right.val;
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator<(
+	MesiType<T, t_m, t_s, t_kg> const& left,
+	MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return left.val < right.val;
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator!=(
+	MesiType<T, t_m, t_s, t_kg> const& left,
+	MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return !(right == left);
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator<=(
+	MesiType<T, t_m, t_s, t_kg> const& left,
+	MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return left < right || left == right;
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator>(
+	MesiType<T, t_m, t_s, t_kg> const& left,
+	MesiType<T, t_m, t_s, t_kg> const& right
+) {
+	return right < left;
+}
+
+template<typename T, int t_m, int t_s, int t_kg>
+constexpr bool operator>=(
+	MesiType<T, t_m, t_s, t_kg> const& right,
+	MesiType<T, t_m, t_s, t_kg> const& left
+) {
+	return left > right || left == right;
+}
+
+/*
+ * Literal operators, to allow quick creation of basic types
+ * Note
+ */
+
+#ifndef MESI_LITERAL_TYPE
+#	define MESI_LITERAL_TYPE float
+#endif
+
+constexpr MesiType<MESI_LITERAL_TYPE, 1, 0, 0> operator "" _m(long double arg) {
+	return MesiType<MESI_LITERAL_TYPE, 1, 0, 0>(arg);
+}
+
+constexpr MesiType<MESI_LITERAL_TYPE, 0, 1, 0> operator "" _s(long double arg) {
+	return MesiType<MESI_LITERAL_TYPE, 0, 1, 0>(arg);
+}
+
+constexpr MesiType<MESI_LITERAL_TYPE, 0, 0, 1> operator "" _kg(long double arg) {
+	return MesiType<MESI_LITERAL_TYPE, 0, 0, 1>(arg);
+}
+
+constexpr MesiType<MESI_LITERAL_TYPE, 1, 0, 0> operator "" _m(unsigned long long arg) {
+	return MesiType<MESI_LITERAL_TYPE, 1, 0, 0>(arg);
+}
+
+constexpr MesiType<MESI_LITERAL_TYPE, 0, 1, 0> operator "" _s(unsigned long long arg) {
+	return MesiType<MESI_LITERAL_TYPE, 0, 1, 0>(arg);
+}
+
+constexpr MesiType<MESI_LITERAL_TYPE, 0, 0, 1> operator "" _kg(unsigned long long arg) {
+	return MesiType<MESI_LITERAL_TYPE, 0, 0, 1>(arg);
 }
--- test.cpp
+++ test.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 
 int main() {
-	constexpr auto xyzzy = MesiType<float, 0, 0, 1>(1);
-	constexpr auto foo = 1._m;
-	constexpr auto bar = 2._m;
-	constexpr auto baz = 1._m + bar;
+	auto constexpr foo = MesiType<float, 0, 0, 1>(1.f);
+
+	constexpr auto xyzzy = MesiType<float, 0, 0, 1>(1.f);
+//	constexpr auto foo = 1._m;
+	constexpr auto bar = 2_m;
+	constexpr auto baz = 1._m - bar;
+
 	std::cout << foo.val << foo.getUnit() << std::endl;
 	std::cout << baz.val << baz.getUnit() << std::endl;
 	std::cout << xyzzy.val << xyzzy.getUnit() << std::endl;
